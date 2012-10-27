0ffe7689ff7ade52097b82d32f91b3d024d27c27
Aldor007 <aldor007@gmail.com>
2012-10-27 11:28:07 +0200

    Nagrywanie

---
+++ kaciubaTests/Gstreamer/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lglib-2.0 -lgobject-2.0 -lgstreamer-0.10 -lgmodule-2.0 -lgthread-2.0 -lrt -lxml2 -lglib-2.0 -lgobject-2.0 -lgstreamer-0.10 -lgmodule-2.0 -lgthread-2.0 -lrt -lxml2
+
